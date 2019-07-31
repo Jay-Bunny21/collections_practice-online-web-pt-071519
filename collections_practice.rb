55d6c34e312c9a4b1719670d7751738c4ac2611c
Jinai Franklin <jinai.franklin@yahoo.com>
2019-07-31 03:25:41 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,8 +13,10 @@ def sort_array_char_count(i)
 end
 
 
-def swap_elements
-  
+def swap_elements(array)
+  array[1] & array [2] = array [2] & array [1]
+end
+return array 
 end
 
 def reverse_array(array)
